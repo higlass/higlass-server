7fdee27d51c13d110d2ce21039626c49acca6e87
Jacob Luber <jluber@trinity.edu>
2016-11-10 13:19:50 -0500

    fixed static file issue in tornado server

--- api/api/settings.py
+++ api/api/settings.py
@@ -136,10 +136,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_ROOT = '/home/ubuntu/higlass-server/api'
+#STATIC_ROOT = '/Users/jacobluber/higlass-server/api/static/'
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+#STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, 'static'),
+#)
 
