91bccc7e83bbcf8baca1f4b6cec2ed71b10693fa
Fritz Lekschas <service@lekschas.de>
2016-12-15 12:08:34 -0500

    PEP8tification. Clean-up.

--- tilesets/tiles.py
+++ tilesets/tiles.py
@@ -1,4 +1,4 @@
-import getter as hgg
+import getter
 import logging
 import numpy as np
 
@@ -8,19 +8,22 @@ logger = logging.getLogger(__name__)
 def makeTile(zoomLevel, x_pos, y_pos, dset):
     info = dset[1]
     divisor = 2 ** zoomLevel
+
     start1 = x_pos * info['max_width'] / divisor
     end1 = (x_pos + 1) * info['max_width'] / divisor
     start2 = y_pos * info['max_width'] / divisor
     end2 = (y_pos + 1) * info['max_width'] / divisor
-    data = hgg.getData3(dset[0], zoomLevel, start1, end1 - 1, start2, end2 - 1)
+
+    data = getter.get_data(
+        dset[0], zoomLevel, start1, end1 - 1, start2, end2 - 1
+    )
 
     df = data[data['genome_start'] >= start1]
     binsize = dset[0].attrs[str(zoomLevel)]
     j = (df['genome_start'].values - start1) // binsize
     i = (df['genome_end'].values - start2) // binsize
     v = np.nan_to_num(df['balanced'].values)
-    # m = (end1 - start1) // binsize
-    # n = (end2 - start2) // binsize
+
     zi = zip(zip(i, j), v)
     tile_bins = dict(zi)
     denseOutputArray = []
