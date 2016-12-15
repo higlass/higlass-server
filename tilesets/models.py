aeb298ce062c1d2cd9ef78194e234130cd94e6bc
Fritz Lekschas <service@lekschas.de>
2016-12-15 12:14:36 -0500

    Update + PEP8tification

--- tilesets/models.py
+++ tilesets/models.py
@@ -1,47 +1,21 @@
 from __future__ import unicode_literals
+
+import slugid
+
 from django.db import models
-from pygments.lexers import get_all_lexers
-from pygments.styles import get_all_styles
-from pygments.lexers import get_lexer_by_name
-from pygments.formatters.html import HtmlFormatter
-from pygments import highlight
-from rest_framework.decorators import api_view, permission_classes
-from tilesets.permissions import IsOwnerOrReadOnly, IsRequestMethodGet
-import uuid
-import logging
-
-LEXERS = [item for item in get_all_lexers() if item[1]]
-LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
-STYLE_CHOICES = sorted((item, item) for item in get_all_styles())
-
-logger = logging.getLogger(__name__)
-
-
-def save(self, *args, **kwargs):
-    """
-    Use the `pygments` library to create a highlighted HTML
-    representation of the code snippet.
-    """
-    lexer = get_lexer_by_name(self.language)
-    # linenos = self.linenos and 'table' or False
-    options = self.title and {'title': self.title} or {}
-    formatter = HtmlFormatter(
-        style=self.style,
-        linenos=linenos,
-        full=True, **options
-    )
-    self.highlighted = highlight(self.code, lexer, formatter)
-    super(tilesets, self).save(*args, **kwargs)
 
 
 class Tileset(models.Model):
     created = models.DateTimeField(auto_now_add=True)
-    # permission_classes = (IsRequestMethodGet,)
-    uuid = models.CharField(max_length=100, unique=True, default=uuid.uuid4)
+    uuid = models.CharField(max_length=100, unique=True, default=slugid.nice)
     processed_file = models.TextField()
     file_type = models.TextField()
+    owner = models.ForeignKey(
+        'auth.User', related_name='tilesets', on_delete=models.CASCADE
+    )
+    private = models.BooleanField(default=False)
+    name = models.TextField(blank=True)
 
     class Meta:
-        # model = Tileset
-        # fields = ('uuid')
         ordering = ('created',)
+        permissions = (('view_tileset', "View tileset"),)
