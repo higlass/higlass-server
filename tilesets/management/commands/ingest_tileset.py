85f972951f0ada0bfef0fe5670a626b49cfacad0
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2017-03-02 15:25:16 -0500

    --name option

--- tilesets/management/commands/ingest_tileset.py
+++ tilesets/management/commands/ingest_tileset.py
@@ -2,6 +2,7 @@ from django.core.management.base import BaseCommand
 import slugid
 import tilesets.models as tm
 import django.core.files.uploadedfile as dcfu
+import os.path as op
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
@@ -10,6 +11,7 @@ class Command(BaseCommand):
         parser.add_argument('--filetype', type=str)
         #parser.add_argument('--coord', default='hg19', type=str)
         parser.add_argument('--uid', type=str)
+        parser.add_argument('--name', type=str)
 
     def handle(self, *args, **options):
         filename = options['filename']
@@ -17,6 +19,7 @@ class Command(BaseCommand):
         filetype = options['filetype']
         #coord = options['coord']
         uid = options.get('uid') or slugid.nice()
+        name = options.get('name') or op.split(filename)[1]
 
         upload_file = open(filename, 'r')
         datafile = dcfu.SimpleUploadedFile(upload_file.name, upload_file.read())
@@ -25,4 +28,5 @@ class Command(BaseCommand):
             filetype=filetype,
             datatype=datatype,
             owner=None,
-            uuid=uid)
+            uuid=uid,
+            name=name)
