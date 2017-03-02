cd1e0e721c8a6fdacc81ab05a7c3262e20b60172
mccalluc <mccallucc+github@gmail.com>
2017-03-02 22:28:44 +0000

    update dense: name not in this response

--- higlass_server/tests.py
+++ higlass_server/tests.py
@@ -26,8 +26,7 @@ class CommandlineTest(unittest.TestCase):
                        [r'"'+id+'.1.1.1":',
                         r'"max_value": 2.0264008045196533',
                         r'"min_value": 0.0',
-                        r'"dense": "JTInPwAAAAAAA',
-                        r'"name": "'+cooler+'"'])
+                        r'"dense": "OjkAAAAAAA'])
 
     def test_cli_huge_upload(self):
         cooler = 'huge.fake.cool'
