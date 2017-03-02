c3cab14ec1095a734b0724b9efd7d657f0993e30
mccalluc <mccalluc@users.noreply.github.com>
2017-03-02 17:38:41 -0500

    Mccalluc/name uploaded files (#35)
    * --name option
    
    * Clean up tests
    
    * update dense: name not in this response

--- higlass_server/tests.py
+++ higlass_server/tests.py
@@ -3,22 +3,30 @@ import subprocess
 
 class CommandlineTest(unittest.TestCase):
     def setUp(self):
+        # TODO: There is probably a better way to clear data from previous test runs. Is it even necessary?
+        # self.assertRun('python manage.py flush --noinput --settings=higlass_server.settings_test')
         pass
-        self.assertRun('python manage.py flush --noinput --settings=higlass_server.settings_test')
 
-    def assertRun(self, command,  output_re=r''):
-        self.assertRegexpMatches(subprocess.check_output(command , shell=True).strip(), output_re)
+    def assertRun(self, command,  output_res=[]):
+        output = subprocess.check_output(command , shell=True).strip()
+        for output_re in output_res:
+            self.assertRegexpMatches(output, output_re)
 
     def test_hello(self):
-        self.assertRun('echo "hello?"', r'hello')
+        self.assertRun('echo "hello?"', [r'hello'])
 
     def test_cli_upload(self):
         cooler = 'dixon2012-h1hesc-hindiii-allreps-filtered.1000kb.multires.cool'
         settings = 'higlass_server.settings_test'
         id = 'cli-test'
         self.assertRun('python manage.py ingest_tileset --filename data/'+cooler+' --datatype matrix --filetype cooler --uid '+id+' --settings='+settings)
+        self.assertRun('curl -s http://localhost:6000/api/v1/tileset_info/?d='+id,
+                       [r'"name": "'+cooler+'"'])
         self.assertRun('curl -s http://localhost:6000/api/v1/tiles/?d='+id+'.1.1.1',
-                       r'\{"'+id+'.1.1.1": \{"max_value": 2.0264008045196533, "min_value": 0.0, "dense": "OjkAAAAAAAA')
+                       [r'"'+id+'.1.1.1":',
+                        r'"max_value": 2.0264008045196533',
+                        r'"min_value": 0.0',
+                        r'"dense": "OjkAAAAAAA'])
 
     def test_cli_huge_upload(self):
         cooler = 'huge.fake.cool'
@@ -27,5 +35,7 @@ class CommandlineTest(unittest.TestCase):
         settings = 'higlass_server.settings_test'
         id = 'cli-huge-test'
         self.assertRun('python manage.py ingest_tileset --filename data/'+cooler+' --datatype foo --filetype bar --uid '+id+' --settings='+settings)
+        self.assertRun('curl -s http://localhost:6000/api/v1/tileset_info/?d='+id,
+                       [r'"name": "'+cooler+'"'])
         self.assertRun('curl -s http://localhost:6000/api/v1/tiles/?d='+id+'.1.1.1',
-                       r'\{"'+id+'.1.1.1":')
+                       [r'"'+id+'.1.1.1"'])
