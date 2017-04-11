8bb66a5b9cff77791b920e3b91c586be29a34cd1
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2017-04-11 15:05:24 -0400

    Added test server IP to ALLOWED_HOSTS

--- higlass_server/settings.py
+++ higlass_server/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'e71$i%^qmf9cml7-ga@r+h3&v$y4c48a&u@%@$8t87z*qz9#ks'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['52.45.229.11', 'localhost', '127.0.0.1', 'higlass.site', 'higlass.io']
+ALLOWED_HOSTS = ['34.199.119.21', '52.45.229.11', 'localhost', '127.0.0.1', 'higlass.site', 'higlass.io']
 
 if 'SITE_URL' in os.environ:
     ALLOWED_HOSTS += [os.environ['SITE_URL']]
