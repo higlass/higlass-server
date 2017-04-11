02fd060fc4c9b9027e6b088d47fd3072bedb8ba6
Fritz Lekschas <service@lekschas.de>
2017-04-11 11:21:41 -0400

    Remove old memcache settings

--- higlass_server/settings.py
+++ higlass_server/settings.py
@@ -171,13 +171,6 @@ DATABASES = {
     }
 }
 
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-        'LOCATION': '127.0.0.1:11211',
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
