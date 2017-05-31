2262c59177cb634c0cc47ced21aaa120d5bf3cb4
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2017-05-31 12:25:04 -0400

    Removed the chroms app and anything associated with it. Chromosome sizes are stored in the tilesets table

--- tilesets/urls.py
+++ tilesets/urls.py
@@ -21,6 +21,8 @@ urlpatterns = [
     url(r'^suggest/$', views.suggest),
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^chrom-sizes/$', views.sizes),
+    url(r'^available-chrom-sizes/$', views.available_chrom_sizes)
     #url(r'^users/$', views.UserList.as_view()),
     #url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view())
 ]
