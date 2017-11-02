88c351b7d223b654d341bd2a13d111fecc060a20
Fritz Lekschas <service@lekschas.de>
2017-11-02 18:23:32 -0400

    Code Stylefy

--- tilesets/tiles.py
+++ tilesets/tiles.py
@@ -1,13 +1,21 @@
 from __future__ import print_function
 
-import cooler
 import cooler.contrib.higlass as cch
 import logging
 import numpy as np
 
 logger = logging.getLogger(__name__)
 
-def make_tiles(hdf_for_resolution, resolution, x_pos, y_pos, transform_type='default', x_width=1, y_width=1):
+
+def make_tiles(
+    hdf_for_resolution,
+    resolution,
+    x_pos,
+    y_pos,
+    transform_type='default',
+    x_width=1,
+    y_width=1
+):
     '''
     Generate tiles for a given location. This function retrieves tiles for
     a rectangular region of width x_width and height y_width
@@ -22,7 +30,7 @@ def make_tiles(hdf_for_resolution, resolution, x_pos, y_pos, transform_type='def
         The starting y position
     cooler_file: string
         The filename of the cooler file to get the data from
-    x_width: int 
+    x_width: int
         The number of tiles to retrieve along the x dimension
     y_width: int
         The number of tiles to retrieve along the y dimension
@@ -48,14 +56,10 @@ def make_tiles(hdf_for_resolution, resolution, x_pos, y_pos, transform_type='def
     '''
 
     data = cch.get_data(
-        hdf_for_resolution, start1, end1 - 1, start2, end2 - 1, 
+        hdf_for_resolution, start1, end1 - 1, start2, end2 - 1,
         transform_type
     )
 
-    #print("data:", data)
-
-    #print("x_width:", x_width)
-    #print("y_width:", y_width)
     # split out the individual tiles
     data_by_tilepos = {}
 
@@ -63,7 +67,7 @@ def make_tiles(hdf_for_resolution, resolution, x_pos, y_pos, transform_type='def
         for y_offset in range(0, y_width):
 
             start1 = (x_pos + x_offset) * tile_size
-            end1 = (x_pos + x_offset+ 1) * tile_size
+            end1 = (x_pos + x_offset + 1) * tile_size
             start2 = (y_pos + y_offset) * tile_size
             end2 = (y_pos + y_offset + 1) * tile_size
 
