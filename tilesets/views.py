6ca80b0f2af52a19c7b413a97e6c1a17b2816a74
Fritz Lekschas <service@lekschas.de>
2017-11-25 21:06:20 -0500

    Fix uploaded view config loading

--- tilesets/views.py
+++ tilesets/views.py
@@ -208,7 +208,7 @@ def sizes(request):
 
     else:
         try:
-            with open(chrom_sizes.datafile, 'r') as f:
+            with open(chrom_sizes.datafile.url, 'r') as f:
                 if res_type == 'json':
                     reader = csv.reader(f, delimiter='\t')
 
