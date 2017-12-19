faf97498c2af6acfff3de918cae006249836d5a6
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2017-12-19 17:29:45 -0500

    Initial version returning read info

--- tilesets/bam_files.py
+++ tilesets/bam_files.py
@@ -51,38 +51,70 @@ def sample_reads(samfile, num_entries=256, entry_length=10000,
     results = []
 
     for pos in poss:
-        print("pos1:", pos)
-        print('cum_seq_lengths', cum_seq_lengths)
+        #print("pos1:", pos)
+        #print('cum_seq_lengths', cum_seq_lengths)
         fnz = np.flatnonzero(cum_seq_lengths >= pos)
 
         if len(fnz) == 0:
             continue
 
-        print('fnz:', fnz)
+        #print('fnz:', fnz)
         seq_num = fnz[0]
         seq_name = samfile.references[seq_num]
-        print("seq_name:", seq_name)
+        #print("seq_name:", seq_name)
         cname = '{}'.format(seq_name)
         
-        print('pos:', pos)
-        print('cum_seq_lengths[seq_num]', cum_seq_lengths[seq_num])
+        #print('pos:', pos)
+        #print('cum_seq_lengths[seq_num]', cum_seq_lengths[seq_num])
         if seq_num > 0:
             pos = pos - cum_seq_lengths[seq_num-1]
-        print("seq_name:", seq_name, 'pos:', pos )
+        #print("seq_name:", seq_name, 'pos:', pos )
         
         reads = samfile.fetch(cname, pos, pos + entry_length)
 
-        print('reads:', reads)
+        #print('reads:', reads)
         for read in reads:
+            query_seq = read.query_sequence
+
+            differences = []
+            try:
+                for counter, (qpos, rpos, ref_base) in enumerate(read.get_aligned_pairs(with_seq=True)):
+                    # inferred from the pysam source code:
+                    # https://github.com/pysam-developers/pysam/blob/3defba98911d99abf8c14a483e979431f069a9d2/pysam/libcalignedsegment.pyx
+                    # and GitHub issue:
+                    # https://github.com/pysam-developers/pysam/issues/163
+                    #print('qpos, rpos, ref_base', qpos, rpos, ref_base)
+                    if rpos is None:
+                        differences += [(qpos, 'I')]
+                    elif qpos is None:
+                        differences += [(counter, 'D')]
+                    elif ref_base.islower():
+                        differences += [(qpos, query_seq[qpos])]
+            except ValueError as ve:
+                # probably lacked an MD string
+                pass
+
+            results += [ [
+                    read.reference_id,
+                    read.reference_start,
+                    read.rlen,
+                    differences
+                    ]]
+
+            '''
             print('read:', read)
             print("dir", dir(read))
             print(read.reference_id)
             print(read.reference_start)
             print(read.rlen)
             print(read.get_tag('MD'))
-            return None
+            print(read.get_reference_sequence())
+            print(read.query_sequence)
+            print(read.get_aligned_pairs(with_seq=True))
+            '''
 
-        results += [len(list(reads))]
+
+            # results += [len(list(reads))]
         
         #samfile.count_coverage(cname, pos, pos + entry_length)
         
