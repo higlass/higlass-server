d94720b6adbbff4d0b877f497d6e27443fc2eae4
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2018-02-01 12:16:57 -0500

    Added new multivec tiles code

--- tilesets/multivec_tiles.py
+++ tilesets/multivec_tiles.py
@@ -1,4 +1,5 @@
 import numpy as np
+import time
 
 def abs2genomic(chromsizes, start_pos, end_pos):
     '''
@@ -54,40 +55,81 @@ def get_tile(f, chromsizes, resolution, start_pos, end_pos, shape):
         the values for the portion of the genome that is visible.
     '''
     binsize = resolution
+    print('binsize:', binsize)
     print('start_pos:', start_pos, 'end_pos:', end_pos)
+    print("length:", end_pos - start_pos)
     print('shape:', shape)
 
+    t0 = time.time()
     arrays = []
+    count = 0
+
+    # keep track of how much data has been returned in bins
+    current_binned_data_position = 0
+    current_data_position = 0
+    
+    num_added = 0
+    total_length = 0
+
     for cid, start, end in abs2genomic([c[1] for c in chromsizes], start_pos, end_pos):
         n_bins = int(np.ceil((end - start) / binsize))
-        print("cid:", cid, 'n_bins:', n_bins)
+        total_length += end - start
         
         try:
+            t1 = time.time()
+
             chrom = chromsizes[cid][0]
             clen = chromsizes[cid][1]
 
-            print('chrom:', chrom)
+            current_data_position += end - start
+
+            count += 1
 
             start_pos = start // binsize
             end_pos = end // binsize
 
-            print('start:', start, 'end:', end)
+            print('current_data_position:', current_data_position)
+            print('current_binned_data_position:', current_binned_data_position)
             print('binsize:', binsize, 'resolution:', resolution)
+
+            if start_pos == end_pos:
+                if current_data_position - current_binned_data_position > 0:
+                    # adding this data as a single bin even though it's not large
+                    # enough to cover one bin
+                    #print('catching up')
+                    end_pos += 1
+                else:
+                    #print('data smaller than the bin size', start, end, binsize)
+                    continue
             
             x = f['resolutions'][str(resolution)]['values'][chrom][start_pos:end_pos]
-            print("x:", x.shape)
+            current_binned_data_position += binsize * (end_pos - start_pos)
+
+            # print("x:", x.shape)
 
             # drop the very last bin if it is smaller than the binsize
             if len(x) > 1 and end == clen and clen % binsize != 0:
-                print("dropping")
+                #print("dropping")
                 x = x[:-1]
+
+            if len(x):
+                num_added += 1
+                print('num_added:', num_added, 'x:', sum(x[0]))
+
+            t2 = time.time()
+
+            # print("time to fetch {}: {}".format(chrom, t2 - t1))
         except IndexError:
             # beyond the range of the available chromosomes
             # probably means we've requested a range of absolute
             # coordinates that stretch beyond the end of the genome
-            print('zeroes')
+            # print('zeroes')
             x = np.zeros((n_bins, shape[1]))
 
         arrays.append(x)
+
+    print("total_length:", total_length)
+    t3 = time.time()
+    # print("total fetch time:", t3 - t0)
 
     return np.concatenate(arrays)
