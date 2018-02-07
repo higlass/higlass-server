cc79ab89f9c0abc7270145bd841d22b0f1df4e6a
Fritz Lekschas <service@lekschas.de>
2018-02-07 15:49:40 -0500

    Add 2d index db querying

--- imtiles/utils.py
+++ imtiles/utils.py
@@ -2,6 +2,51 @@ import os
 import sqlite3
 
 
+def get_features(db_file_path, zoom, x_from, x_to, y_from, y_to):
+    '''
+    Retrieve them features from the database.
+
+    Parameters
+    ----------
+    db_file_path: str
+        The filename of the sqlite db file
+    zoom: int
+        The zoom level
+    x_from: foat
+        The start of the longitude
+    x_to: foat
+        The end of the longitude
+    y_from: foat
+        The start of the latitude
+    y_to: foat
+        The end of the latitude
+
+    Returns
+    -------
+    rows: list
+        Rows of the database matching the query
+    '''
+
+    conn = sqlite3.connect(db_file_path)
+    c = conn.cursor()
+
+    query = '''
+    SELECT
+        fromX, toX, fromY, toY, chrOffset, importance, fields, uid
+    FROM
+        intervals,position_index
+    WHERE
+        intervals.id=position_index.id AND
+        zoomLevel <= ? AND
+        rToX >= ? AND
+        rFromX <= ? AND
+        rToY >= ? AND
+        rFromY <= ?
+    '''
+
+    return c.execute(query, (zoom, x_from, x_to, y_from, y_to)).fetchall()
+
+
 def get_tileset_info(tileset):
     if not os.path.isfile(tileset):
         return {
