f3c5458e74c457705069c7961404e038e1964cbf
Peter Kerpedjiev <pkerpedjiev@zymergen.com>
2018-12-23 11:08:45 -0500

    Removed extraneous parameters from simple-httpfs

--- docker-context/httpfs.sh
+++ docker-context/httpfs.sh
@@ -4,6 +4,6 @@ mkdir -p $HTTPFS_HTTP_DIR
 mkdir -p $HTTPFS_HTTPS_DIR
 mkdir -p $HTTPFS_FTP_DIR
 
-simple-httpfs.py $HTTPFS_HTTPS_DIR https --lru-capacity 1000 --disk-cache-dir /data/disk-cache --disk-cache-size 4294967296
-simple-httpfs.py $HTTPFS_HTTP_DIR http --lru-capacity 1000 --disk-cache-dir /data/disk-cache --disk-cache-size 4294967296
-simple-httpfs.py $HTTPFS_FTP_DIR ftp --lru-capacity 1000 --disk-cache-dir /data/disk-cache --disk-cache-size 4294967296
+simple-httpfs.py $HTTPFS_HTTPS_DIR --lru-capacity 1000 --disk-cache-dir /data/disk-cache --disk-cache-size 4294967296
+simple-httpfs.py $HTTPFS_HTTP_DIR --lru-capacity 1000 --disk-cache-dir /data/disk-cache --disk-cache-size 4294967296
+simple-httpfs.py $HTTPFS_FTP_DIR --lru-capacity 1000 --disk-cache-dir /data/disk-cache --disk-cache-size 4294967296
