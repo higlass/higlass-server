82cc2b2068e1eee5793c1d0f8321bbbff3bbec87
Nezar <nabdennur@gmail.com>
2019-05-07 12:48:07 -0400

    Delete backing media file on removal of a tileset

--- tilesets/models.py
+++ tilesets/models.py
@@ -5,6 +5,12 @@ import django.contrib.auth.models as dcam
 import slugid
 
 from django.db import models
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
+
+
+def decoded_slugid():
+    return slugid.nice()
 
 
 class ViewConf(models.Model):
@@ -23,40 +29,42 @@ class ViewConf(models.Model):
         '''
         return "Viewconf [uuid: {}]".format(self.uuid)
 
-def decoded_slugid():
-    return slugid.nice()
 
 class Project(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     last_viewed_time = models.DateTimeField(default=django.utils.timezone.now)
 
-    owner = models.ForeignKey(dcam.User, on_delete=models.CASCADE, blank=True, null=True)
+    owner = models.ForeignKey(
+        dcam.User, on_delete=models.CASCADE, blank=True, null=True)
     name = models.TextField(unique=True)
     description = models.TextField(blank=True)
-    uuid = models.CharField(max_length=100, unique=True, default=decoded_slugid)
+    uuid = models.CharField(max_length=100, unique=True,
+                            default=decoded_slugid)
     private = models.BooleanField(default=False)
 
     class Meta:
         ordering = ('created',)
         permissions = (('read', "Read permission"),
-                ('write', 'Modify tileset'),
-                ('admin', 'Administrator priviliges'),
-            )
+                       ('write', 'Modify tileset'),
+                       ('admin', 'Administrator priviliges'),
+                       )
 
     def __str__(self):
         return "Project [name: " + self.name + "]"
 
+
 class Tileset(models.Model):
     created = models.DateTimeField(auto_now_add=True)
 
-    uuid = models.CharField(max_length=100, unique=True, default=decoded_slugid)
+    uuid = models.CharField(max_length=100, unique=True,
+                            default=decoded_slugid)
 
     # processed_file = models.TextField()
     datafile = models.FileField(upload_to='uploads')
     filetype = models.TextField()
     datatype = models.TextField(default='unknown', blank=True, null=True)
     project = models.ForeignKey(Project, on_delete=models.CASCADE,
-            blank=True, null=True)
+                                blank=True, null=True)
     description = models.TextField(blank=True)
 
     coordSystem = models.TextField()
@@ -73,13 +81,21 @@ class Tileset(models.Model):
     class Meta:
         ordering = ('created',)
         permissions = (('read', "Read permission"),
-                ('write', 'Modify tileset'),
-                ('admin', 'Administrator priviliges'),
-            )
+                       ('write', 'Modify tileset'),
+                       ('admin', 'Administrator priviliges'),
+                       )
 
     def __str__(self):
         '''
         Get a string representation of this model. Hopefully useful for the
         admin interface.
         '''
-        return "Tileset [name: {}] [ft: {}] [uuid: {}]".format(self.name, self.filetype, self.uuid)
+        return "Tileset [name: {}] [ft: {}] [uuid: {}]".format(
+            self.name, self.filetype, self.uuid)
+
+
+@receiver(post_delete, sender=Tileset)
+def tileset_on_delete(sender, instance, **kwargs):
+
+    if not instance.datafile.name.endswith('..'):
+        instance.datafile.delete(False)
